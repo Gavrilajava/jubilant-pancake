6e8a9d7f9fcaae78e0b1b2e5d64187224056b5d3
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-03-23 17:37:51 -0500

    fixed index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/:user_secret_id', to: 'application#index'
-  get '/', to: 'application#index'
   get '/channels/:user_secret_id/:last_message_id', to: 'application#messages'
   post '/messages', to: 'application#new_message'
   post '/channels', to: 'application#new_channel'
